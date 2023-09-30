69681b024729d61a1f4c8ade16b1541297e4d629
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-09-30 20:07:13 +0530

    comment-details

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -2,7 +2,9 @@
 let stringArr = ['one', 'hey', 'Souvik'];
 let guitars = ['Strat', 'Les Paul', 5150];
 let mixedData = ['EVH', 1984, true];
+// we cannot add any data of datatype other than string in stringArr.
 stringArr[0] = 'Developer';
 stringArr.push('fogg');
+// we can add only string and numerical datatype to this guitars variable.
 guitars[0] = 1984;
 guitars.unshift('hey');
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -4,8 +4,10 @@ let guitars = ['Strat', 'Les Paul', 5150];
 
 let mixedData = ['EVH', 1984, true]
 
+// we cannot add any data of datatype other than string in stringArr.
 stringArr[0] = 'Developer';
 stringArr.push('fogg');
 
+// we can add only string and numerical datatype to this guitars variable.
 guitars[0] = 1984;
 guitars.unshift('hey');
