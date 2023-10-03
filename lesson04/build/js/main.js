84b89ff6e1e0bfec571c8b51681b923970590eb2
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-10-03 18:27:37 +0530

    type-imp15

--- lesson04/build/js/main.js
+++ lesson04/build/js/main.js
@@ -3,3 +3,10 @@
 let myName;
 let userName;
 userName = "MERN";
+// function
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
--- lesson04/src/main.ts
+++ lesson04/src/main.ts
@@ -18,3 +18,13 @@ let myName: "souvik";
 let userName: "Souvik" | "Developa" | "MERN";
 
 userName = "MERN"
+
+// function
+
+const add = (a: number, b: number): number => {
+    return a + b;
+}
+
+const logMsg = (message: any): void => {
+    console.log(message);
+}
