7b9d6ae255943961de5f32dfb9b81391608aef7d
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-10-05 18:27:02 +0530

    type-imp22

--- lesson05/build/js/main.js
+++ lesson05/build/js/main.js
@@ -1,23 +1,6 @@
 "use strict";
-// convert to more or less specific 
-let a = 'hello';
-let b = a; // less specific 
-let c = a; // more specific 
-let d = 'world';
-let e = 'world';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-// Be careful! TS sees no problem - but a string is returned
-let nextVal = addOrConcat(2, 2, 'concat');
-//10 as string
-10;
-// The DOM 
-const img = document.querySelector('img');
-const myImg = document.getElementById('#img');
-const nextImg = document.getElementById('#img');
-img.src;
-myImg.src;
+let myFullName = "Souvik Roy";
+let myFirstName = "Souvik";
+let myLastName = "Roy";
+myFullName = myFirstName + myLastName;
+console.log(myFullName);
