ed7e20a9c7b3bb5b226bceee6c029e5f4c1b9f96
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-18 18:24:08 +0530

    void func type

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -1,5 +1,14 @@
-function tsFunc(par1) {
-    return par1 + 1;
+// function tsFunc(par1: number) {
+//     return par1 + 1;
+// }
+// const res = tsFunc(3);
+// console.log(res);
+// arrow function
+// const addone = (num: number) => num + 1;
+// const response = addone(5);
+// console.log(response);
+function printMessage(message) {
+    console.log("This is my ".concat(message));
 }
-var res = tsFunc(3);
-console.log(res);
+;
+printMessage("text");
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,7 +1,20 @@
-function tsFunc(par1: number) {
-    return par1 + 1;
-}
+// function tsFunc(par1: number) {
+//     return par1 + 1;
+// }
 
-const res = tsFunc(3);
+// const res = tsFunc(3);
 
-console.log(res);
+// console.log(res);
+
+// arrow function
+
+// const addone = (num: number) => num + 1;
+
+// const response = addone(5);
+// console.log(response);
+
+function printMessage(message: string): void {
+    console.log(`This is my ${message}`);
+};
+
+printMessage("text");
