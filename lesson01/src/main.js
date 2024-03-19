560ed4ac87f8fdad5200d15122e94c46c4996b5e
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-19 17:38:05 +0530

    enum custom data types

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -1,7 +1,3 @@
-var password = 22;
-var user = {
-    email: "souvik@gmail.com",
-    password: "1234567898"
-};
-var items = [1, 2, 3, 4, 5, 6, "souvik"];
-console.log(items);
+var color;
+color = "red";
+console.log(color);
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,20 +1,4 @@
-const password: string | number = 22;
+let color: "red" | "blue" | "green";
 
-type UserInfo = {
-    first: string,
-    last: string,
-    age: number
-}
-
-type AccountDetails = {
-    email: string,
-    password: string
-}
-
-const user: UserInfo | AccountDetails = {
-    email: "souvik@gmail.com",
-    password: "1234567898"
-}
-
-const items: (number | string)[] = [1, 2, 3, 4, 5, 6, "souvik"]
-console.log(items)
+color = "red";
+console.log(color);
