b616234bb1d003a9008d887442127cd7a51a07e0
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-20 19:05:59 +0530

    interfaces advanced concepts

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -2,22 +2,21 @@ function greet(person) {
     console.log("Hello, ".concat(person.firstName, " ").concat(person.lastName));
     person.sayHello();
 }
-;
 var john = {
     firstName: "John",
     lastName: "Doe",
     age: 20,
     sayHello: function () {
-        console.log("Hi There!");
-    },
+        console.log("Hi There");
+    }
 };
 var souvik = {
     firstName: "Souvik",
     lastName: "Roy",
     age: 22,
     sayHello: function () {
-        console.log("Web Developer");
-    },
+        console.log("Web3 Developer");
+    }
 };
 greet(john);
 greet(souvik);
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -2,21 +2,21 @@ interface Person {
     firstName: string,
     lastName: string,
     age: number,
-    sayHello(): void
+    sayHello(): void,
 }
 
 function greet(person: Person) {
     console.log(`Hello, ${person.firstName} ${person.lastName}`);
     person.sayHello();
-};
+}
 
 const john: Person = {
     firstName: "John",
     lastName: "Doe",
     age: 20,
     sayHello() {
-        console.log("Hi There!");
-    },
+        console.log(`Hi There`);
+    }
 }
 
 const souvik: Person = {
@@ -24,8 +24,8 @@ const souvik: Person = {
     lastName: "Roy",
     age: 22,
     sayHello() {
-        console.log("Web Developer");
-    },
+        console.log("Web3 Developer");
+    }
 }
 
 greet(john);
