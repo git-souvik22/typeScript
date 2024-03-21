e3a6219a97cdf09539107664413b6686a6278a72
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-21 18:54:45 +0530

    generics 2nd

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -1,5 +1,10 @@
 function printNumber(item, defaultValue) {
     return [item, defaultValue];
 }
+function printString(item, defaultValue) {
+    return [item, defaultValue];
+}
 var num = printNumber(12, 20);
+var str = printString("Hello", "World");
 console.log(num);
+console.log(str);
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -2,6 +2,11 @@ function printNumber(item: number, defaultValue: number): [number, number] {
     return [item, defaultValue];
 }
 
-const num = printNumber(12, 20);
+function printString(item: string, defaultValue: string): [string, string] {
+    return [item, defaultValue];
+}
 
+const num = printNumber(12, 20);
+const str = printString("Hello", "World");
 console.log(num);
+console.log(str);
