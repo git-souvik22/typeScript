1b55570905f6b8f2cdcd0c1e903f5c13177d10f5
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-22 19:36:31 +0530

    get and set generic initial value

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -5,10 +5,14 @@ var Box = /** @class */ (function () {
     Box.prototype.getContent = function () {
         return this.content;
     };
+    ;
     Box.prototype.setContent = function (newContent) {
         this.content = newContent;
     };
+    ;
     return Box;
 }());
-var stringBox = new Box("Hello, TypeScript");
+var stringBox = new Box("Hello, Typescript");
+console.log(stringBox.getContent());
+stringBox.setContent("New Value Added");
 console.log(stringBox.getContent());
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,17 +1,17 @@
-class Box<T>{
-    private content: T
+class Box<T> {
+    private content: T;
     constructor(initialContent: T) {
         this.content = initialContent;
     }
-
     getContent(): T {
         return this.content;
-    }
-
+    };
     setContent(newContent: T): void {
         this.content = newContent;
-    }
+    };
 }
 
-const stringBox = new Box<string>("Hello, TypeScript");
+const stringBox = new Box<string>("Hello, Typescript");
+console.log(stringBox.getContent())
+stringBox.setContent("New Value Added");
 console.log(stringBox.getContent());
