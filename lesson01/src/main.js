4c308b74c3f176d00a6df0ea1f32cd3bda991955
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-22 18:24:45 +0530

    generic array string conditional logging

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -4,3 +4,6 @@ function filterArray(array, condition) {
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var evenNumbers = filterArray(numberArray, function (num) { return num % 2 === 0; });
 console.log(evenNumbers);
+var stringArr = ["apple", "banana", "cherry", "date"];
+var shortWords = filterArray(stringArr, function (word) { return word.length < 6; });
+console.log(shortWords);
