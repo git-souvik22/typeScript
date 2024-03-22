f2023d3321690a5303d8ab0c365c4472f87e30fb
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-22 18:46:14 +0530

    numberArray condition revise

--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,11 +1,7 @@
 function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
     return array.filter((item) => condition(item));
-}
+};
 
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
 console.log(evenNumbers);
-
-const stringArr = ["apple", "banana", "cherry", "date"];
-const shortWords = filterArray<string>(stringArr, (word) => word.length < 6);
-console.log(shortWords);
